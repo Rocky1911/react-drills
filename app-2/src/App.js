a462ec8749e227ac674c9b581bcd6e903e992e5b
Rockydonthurtem <charrisq@hotmail.com>
2018-08-14 16:17:48 -0500

    update2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      arr: [1, 2, 3, 4]
-    }
+      one: [1, 2, 3, 4]
+    };
   }
   render() {
-    return (
-      <div className="App">
-        {this.state.arr}
-      </div>
-    );
+    const { one } = this.state;
+    let bond = one.map((e, i) => {
+      return <div key={i}>{e}</div>;
+    });
+    return <div className="App">{bond}</div>;
   }
 }
 
